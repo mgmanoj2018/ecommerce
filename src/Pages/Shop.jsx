3fd9f45a487711916aebf4746ea5845179ace4f6
mgmanoj2018 <mgmanoj2018@gmail.com>
2023-10-25 18:30:20 +0530

    add new fetures

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollectins from '../Components/NewCllections/NewCollectins'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 function Shop() {
   return (
     <>
-    <Hero/>
-    <Popular/>
-    <Offers/>
+        <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewsLetter/>
+      <NewCollectins/>
+     
+    
     </>
  
   )
